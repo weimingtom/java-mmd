76ca35c83a5c44bf0a5a87d977cfd4d048489fef
satoshiyazawa9 <satoshiyazawa9@gmail.com>
2011-12-23 10:52:14 +0000

    GWTコードを作成。

---
+++ mmd-gwt/src/net/yzwlab/gwtmmd/client/io/FileReadBuffer.java
@@ -0,0 +1,129 @@
+package net.yzwlab.gwtmmd.client.io;
+
+import net.yzwlab.javammd.IReadBuffer;
+import net.yzwlab.javammd.ReadException;
+import net.yzwlab.javammd.io.AbstractReadBuffer;
+
+import org.vectomatic.arrays.ArrayBuffer;
+import org.vectomatic.arrays.DataView;
+import org.vectomatic.arrays.Uint8Array;
+
+public class FileReadBuffer extends AbstractReadBuffer {
+
+	private DataView view;
+
+	private int position;
+
+	private boolean le;
+
+	public FileReadBuffer(ArrayBuffer remain) {
+		if (remain == null) {
+			throw new IllegalArgumentException();
+		}
+		this.view = DataView.createDataView(remain);
+		this.position = 0;
+		this.le = true;
+	}
+
+	@Override
+	public IReadBuffer createFromByteArray(byte[] data) throws ReadException {
+		if (data == null) {
+			throw new IllegalArgumentException();
+		}
+		Uint8Array array = Uint8Array.createUint8Array(data.length);
+		for (int i = 0; i < data.length; i++) {
+			array.set(i, data[i]);
+		}
+		return new FileReadBuffer(array.getBuffer());
+	}
+
+	@Override
+	public boolean isEOF() {
+		if (position >= view.getByteLength()) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public byte readByte() throws ReadException {
+		try {
+			return view.getInt8(position);
+		} finally {
+			position++;
+		}
+	}
+
+	@Override
+	public short readUByte() throws ReadException {
+		try {
+			return view.getUint8(position);
+		} finally {
+			position++;
+		}
+	}
+
+	@Override
+	public short readShort() throws ReadException {
+		try {
+			return view.getInt16(position, le);
+		} finally {
+			position += 2;
+		}
+	}
+
+	@Override
+	public int readUShort() throws ReadException {
+		try {
+			return view.getUint16(position, le);
+		} finally {
+			position += 2;
+		}
+	}
+
+	@Override
+	public int readInteger() throws ReadException {
+		try {
+			return view.getInt32(position, le);
+		} finally {
+			position += 4;
+		}
+	}
+
+	@Override
+	public long readUInt() throws ReadException {
+		try {
+			return view.getUint32(position, le);
+		} finally {
+			position += 4;
+		}
+	}
+
+	@Override
+	public long readLong() throws ReadException {
+		try {
+			return view.getInt32(position, le);
+		} finally {
+			position += 4;
+		}
+	}
+
+	@Override
+	public float readFloat() throws ReadException {
+		try {
+			return view.getFloat32(position, le);
+		} finally {
+			position += 4;
+		}
+	}
+
+	@Override
+	public double readDouble() throws ReadException {
+		try {
+			return view.getFloat64(position, le);
+		} finally {
+			position += 8;
+		}
+	}
+
+}
