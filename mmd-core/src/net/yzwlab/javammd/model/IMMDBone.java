5759c0fddd83358fd684fa2cde8537ba380ce828
satoshiyazawa9 <satoshiyazawa9@gmail.com>
2011-12-24 06:24:08 +0000

    内部情報アクセス用のインタフェースを追加。

---
+++ mmd-core/src/net/yzwlab/javammd/model/IMMDBone.java
@@ -0,0 +1,15 @@
+package net.yzwlab.javammd.model;
+
+/**
+ * ボーンの抽象です。
+ */
+public interface IMMDBone {
+
+	/**
+	 * 名前を取得します。
+	 * 
+	 * @return 名前。
+	 */
+	public byte[] getName();
+
+}
---
+++ mmd-core/src/net/yzwlab/javammd/model/IMMDMaterial.java
@@ -0,0 +1,15 @@
+package net.yzwlab.javammd.model;
+
+/**
+ * マテリアルの抽象です。
+ */
+public interface IMMDMaterial {
+
+	/**
+	 * 頂点数を取得します。
+	 * 
+	 * @return 頂点数。
+	 */
+	public int getVertexCount();
+
+}
