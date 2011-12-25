7b535a778e44220d4cfee27579e6f3101b591ffe
satoshiyazawa9@gmail.com <satoshiyazawa9@gmail.com@457ca513-3043-ff8a-88b9-fbcd6411ba36>
2011-12-25 11:30:15 +0000

    アニメーション機能を実装。

--- trunk/mmd-jogl/src/net/yzwlab/javammd/jogl/MMDDrawer.java
+++ trunk/mmd-jogl/src/net/yzwlab/javammd/jogl/MMDDrawer.java
@@ -20,27 +20,26 @@ public class MMDDrawer implements GLEventListener, IDataMutex {
 
 	private File baseDir;
 
-	private float[] offset;
-
-	private int index;
+	private GLCanvas canvas;
 
 	private MMDModel model;
 
 	private boolean loaded;
 
-	public MMDDrawer(File baseDir, GLCanvas canvas, float[] offset, int index,
-			MMDModel model, long baseTime) {
+	private Long baseTime;
+
+	public MMDDrawer(File baseDir, GLCanvas canvas, MMDModel model,
+			long baseTime) {
 		this.baseDir = baseDir;
-		this.offset = offset;
-		this.index = index;
+		this.canvas = canvas;
 		this.model = model;
 		this.loaded = false;
+		this.baseTime = null;
 	}
 
 	@Override
 	public void reshape(GLAutoDrawable glautodrawable, int x, int y, int width,
 			int height) {
-		System.out.println("reshape: " + index);
 		GL2 gl2 = glautodrawable.getGL().getGL2();
 		gl2.glMatrixMode(GL2.GL_PROJECTION);
 		gl2.glLoadIdentity();
@@ -48,8 +47,8 @@ public class MMDDrawer implements GLEventListener, IDataMutex {
 		GLU glu = new GLU();
 		glu.gluPerspective(30.0, (double) width / (double) height, 1.0, 100.0);
 		gl2.glTranslated(0.0, 0.0, -5.0);
-		glu.gluLookAt(3.0 + offset[index], 2.0 + 14.0, 5.0 + 10.0, 0.0,
-				0.0 + 14.0, 0.0, 0.0, 1.0, 0.0);
+		glu.gluLookAt(3.0, 2.0 + 14.0, 5.0 + 10.0, 0.0, 0.0 + 14.0, 0.0, 0.0,
+				1.0, 0.0);
 
 		// coordinate system origin at lower left with width and height same as
 		// the window
@@ -87,73 +86,51 @@ public class MMDDrawer implements GLEventListener, IDataMutex {
 		model.SetScale(1.0f);
 		// model.SetFace("‚É‚±");
 		// model.SetBoneVisible(30, false);
-		model.UpdateAsync(this, 32.0f * 20);
 
-		gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+		long beginTime = System.currentTimeMillis();
+		try {
+			gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 
-		gl2.glMatrixMode(GL2.GL_MODELVIEW);
-		gl2.glLoadIdentity();
+			gl2.glMatrixMode(GL2.GL_MODELVIEW);
+			gl2.glLoadIdentity();
 
-		gl2.glEnable(GL.GL_DEPTH_TEST);
+			gl2.glEnable(GL.GL_DEPTH_TEST);
 
-		gl2.glColor3d(1.0, 1.0, 1.0);
-		gl2.glTranslatef(0.0f, -0.0f, 0.0f);
+			gl2.glColor3d(1.0, 1.0, 1.0);
+			gl2.glTranslatef(0.0f, -0.0f, 0.0f);
 
-		model.DrawAsync(this, jogl);
+			model.DrawAsync(this, jogl);
 
-		gl2.glDisable(GL.GL_DEPTH_TEST);
-		gl2.glFlush();
+			gl2.glDisable(GL.GL_DEPTH_TEST);
+			gl2.glFlush();
+		} finally {
+			long dur = System.currentTimeMillis() - beginTime;
+			System.out.println("Frame: duration=" + dur + "msec.");
+		}
 
-		// gl2.glReadBuffer(GL2.GL_BACK);
-		//
-		// int width = glautodrawable.getWidth();
-		// int height = glautodrawable.getHeight();
-		// ByteBuffer pixels = ByteBuffer.allocate(width * height * 3);
-		// gl2.glPixelStorei(GL2.GL_PACK_ALIGNMENT, 1);
-		// gl2.glReadPixels(0, 0, width, height, GL2.GL_RGB,
-		// GL2.GL_UNSIGNED_BYTE,
-		// pixels);
-		//
-		// BufferedImage img = new BufferedImage(width, height,
-		// BufferedImage.TYPE_3BYTE_BGR);
-		// byte[] data = new byte[3];
-		// for (int y = 0; y < height; y++) {
-		// for (int x = 0; x < width; x++) {
-		// pixels.position(x * 3 + (height - y - 1) * width * 3);
-		// pixels.get(data, 0, 3);
-		// img.setRGB(x, y, ((data[0] & 0xff) << 16)
-		// | ((data[1] & 0xff) << 8) | (data[2] & 0xff)
-		// | (0xff << 24));
-		// }
-		// }
-		// try {
-		// ImageIO.write(img, "png", new File("test" + index + ".png"));
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-		// if (index + 1 < offset.length) {
-		// index++;
-		// System.out.println("reshape: " + index);
-		// OneTriangle.setup(glautodrawable.getGL().getGL2(), width, height,
-		// offset[index]);
-		// canvas.repaint();
-		// }
-
-		// TODO
-		// long curTime = System.currentTimeMillis() - baseTime;
-		// float frame = (float) (((double) curTime) / 100.0);
-		// int frameCount = model.;
-		// while (frame > frameCount) {
-		// frame -= frameCount;
-		// }
-		// final float nextFrame = frame;
-		// (new Thread(new Runnable() {
-		// @Override
-		// public void run() {
-		// model.update(nextFrame);
-		// canvas.repaint();
-		// }
-		// })).start();
+		if (baseTime == null) {
+			baseTime = System.currentTimeMillis();
+		}
+		double curTime = (System.currentTimeMillis() - baseTime) / 1000.0;
+		float frame = (float) (curTime * 20.0);
+		Integer frameCount = model.getMaxFrame();
+		if (frameCount == null) {
+			return;
+		}
+		if (frameCount == 0) {
+			return;
+		}
+		while (frame > frameCount) {
+			frame -= frameCount;
+		}
+		final float nextFrame = frame;
+		(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				model.UpdateAsync(MMDDrawer.this, nextFrame);
+				canvas.repaint();
+			}
+		})).start();
 	}
 
 	@Override
